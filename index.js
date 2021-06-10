c869ef823baeb56e1cff86a6d08901614ae27bc1
Rodrigo Guerrero <guerrero.rodrigo14@gmail.com>
2021-06-10 11:04:20 -0400

    data changed to works with front application

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ app.use(cors())
 let notes = [
   {
     id: 1,
-    name: 'Karen',
-    title: 'Amorcito',
+    content: 'HTML is easy',
+    date: '2019-05-30T17:30:31.098Z',
     important: true
   },
   {
     id: 2,
-    name: 'Michel',
-    title: 'Negro Garka',
+    content: 'Browser can execute only JavaScript',
+    date: '2019-05-30T18:39:34.091Z',
     important: false
   },
   {
     id: 3,
-    name: 'Miguel',
-    title: 'Gordo Forro',
-    important: false
+    content: 'GET and POST are the most important methods of HTTP protocol',
+    date: '2019-05-30T19:20:14.298Z',
+    important: true
   }
 ]
 
